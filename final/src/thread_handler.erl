b43aa852ee73374e9576fb74d1bd3a6502dcac86
Eldon <Birdheldon@gmail.com>
2025-06-15 22:41:41 -0600

    started work

---
+++ final/src/thread_handler.erl
@@ -0,0 +1,104 @@
+﻿%%%-------------------------------------------------------------------
+%%% @author Birdh
+%%% @copyright (C) 2025, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. Jun 2025 20:34
+%%%-------------------------------------------------------------------
+-module(thread_handler).
+-author("Birdh").
+
+%% API
+-export([]).
+
+
+
+
+
+
+
+
+
+
+
+
+
+thread(UserID) when not is_list(UserID) ->
+  % Initial Setup
+  thread([] ++ [{UserID, create_thread, "Server Has Started Thread", "01/01/25/00/00"}]);
+thread(State) ->
+  receive {Sender, {User_id, Command, Message, Time}} ->
+    
+    % You would add user checking and verification here i think.
+    
+    % perform the command
+    
+    if(Command == "comment") ->
+      
+    
+
+    thread(State)
+  end.
+  
+  
+
+
+
+find_thread([], _) ->
+  "DNE";
+find_thread([{Current_PID, Current_Thread_Id} | _], Thread_ID) when Current_Thread_Id == Thread_ID ->
+  Current_PID;
+find_thread([_ | Remainder], Thread_ID) ->
+  find_thread(Remainder, Thread_ID).
+
+
+thread_registry(Registry) ->
+    receive { Sender, Command, Args} ->
+        
+        result = Command(Args),
+        
+        thread_registry(Registry)
+        
+      
+      end.
+  
+
+% Registry Commands
+
+
+% First ever time creating this thread
+create_thread(User_ID) ->
+  spawn(fun() -> thread(#{[]}) end).
+.
+
+% Try to start up an old thread...?
+start_thread(Registry, Thread_ID) ->
+  result = find_thread(Registry, Thread_ID),
+  if(result == "DNE") ->
+    
+    .
+    
+    
+get_thread_history(Thread_ID) ->
+
+
+
+% Thread Commands
+
+
+
+
+% MSG -> {User_id, "Command", Comment, TimeStamp}
+post_comment(Thread, MSG) ->
+
+  
+  
+  
+  
+.
+
+get_thread_data() ->
+
+.
+  
---
+++ final/src/thread_sup.erl
@@ -0,0 +1,26 @@
+﻿%%%-------------------------------------------------------------------
+%%% @author Birdh
+%%% @copyright (C) 2025, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. Jun 2025 20:34
+%%%-------------------------------------------------------------------
+-module(thread_sup).
+-author("Birdh").
+
+%% API
+-export([]).
+
+
+get_threads() ->
+  
+  .
+
+find_thread(Given_ID) ->
+  
+  .
+
+create_thread() ->
+  spawn(fun() -> memos(#{}) end).
+  .
