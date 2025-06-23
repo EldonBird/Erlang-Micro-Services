eff241ea6259d8aedc4c003bac1b4802cf4763d4
Eldon <Birdheldon@gmail.com>
2025-06-23 12:11:10 -0600

    Monads

---
+++ final/src/monads.erl
@@ -0,0 +1,26 @@
+﻿%%%-------------------------------------------------------------------
+%%% @author Birdh
+%%% @copyright (C) 2025, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 23. Jun 2025 12:04
+%%%-------------------------------------------------------------------
+-module(monads).
+-author("Birdh").
+
+%% API
+-export([]).
+
+
+unit(Value) ->
+  {Value, []}.
+
+
+bind ({Value, Log}, Fun) ->
+  {NewVal, NewLog} = Fun (Value),
+  {NewVal, Log ++ NewLog}.
+
+
+add_num(Value) ->
+  {Value + 5, ["Add5" ++ [Value]]}.
