5add1c4b6085a91685d823af9ea7c78a12d1a82a
Eldon <Birdheldon@gmail.com>
2025-06-25 11:52:30 -0600

    awoieh

--- final/src/Completed/functors.erl
+++ final/src/Completed/functors.erl
@@ -4,39 +4,18 @@
 %%% @doc
 %%%
 %%% @end
-%%% Created : 02. Jun 2025 1:54 PM
+%%% Created : 17. Jun 2025 2:10 PM
 %%%-------------------------------------------------------------------
 -module(functors).
 -author("eldon").
 
-
 %% API
--export([map/2, test/0]).
+-export([better_work/3]).
 
-% Define the binary tree structure using tagged tuples
-% tree() = empty | {node, Value, LeftTree, RightTree}
+better_work(Fun, ok, Value) ->
+  {ok, Fun(Value)};
+better_work(_, error, Reason) ->
+  {error, Reason}.
 
-% Map function: applies Fun to each node's Value
-map(empty, _Fun) ->
-  empty;
-map({node, Value, Left, Right}, Fun) ->
-  NewLeft = map(Left, Fun),
-  NewRight = map(Right, Fun),
-  {node, Fun(Value), NewLeft, NewRight}.
 
-% Test function to demonstrate the map
-test() ->
-  % Create a sample binary tree
-  Tree = {node, 1,
-    {node, 2, empty, empty},
-    {node, 3, empty, empty}},
 
-  % Define a fun to double the node's value
-  DoubleFun = fun(X) -> X * 2 end,
-
-  % Apply map
-  NewTree = map(Tree, DoubleFun),
-
-  % Print original and new trees
-  io:format("Original Tree: ~p~n", [Tree]),
-  io:format("Mapped Tree: ~p~n", [NewTree]).
---
+++ final/src/queue.erl
@@ -0,0 +1,62 @@
+﻿%%%-------------------------------------------------------------------
+%%% @author Birdh
+%%% @copyright (C) 2025, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. Jun 2025 11:47
+%%%-------------------------------------------------------------------
+-module(queue).
+-author("Birdh").
+
+%% API
+-export([]).
+
+
+empty({[], []}) ->
+    true;
+empty(_) ->
+    false.
+
+
+enqueue({Front, Rear}, New) ->
+  {Front, [New | Rear]}.
+
+
+dequeue({[], []}) ->
+  {[], []};
+dequeue({[], Rear}) ->
+  dequeue({lists:reverse(Rear), []});
+dequeue({[_H | T], Rear}) ->
+  {T, Rear}.
+
+
+head({[], []}) -> nil;
+head({[], Rear}) ->
+  case lists:reverse(Rear) of
+    [] -> nil;
+    [H | _] -> H
+  end;
+head({[H | _], _}) -> H.
+
+
+tail({[], []}) -> nil;
+tail({[], Rear}) ->
+  case Rear of
+    [] -> nil;
+    [H | _] -> H
+  end;
+tail({Front, []}) ->
+  case lists:reverse(Front) of
+    [] -> nil;
+    [H | _] -> H
+  end;
+tail({_, [H | _]}) -> H.
+
+
+toList({Front, Rear}) ->
+  Front ++ lists:reverse(Rear).
+
+
+fromList(List) ->
+  {List, []}.
